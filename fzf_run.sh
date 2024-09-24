ee716292f2f20f9b7ca45d5b43b940aa0a2f34f9
thevimmer <amienprivate96@gmail.com>
2024-09-24 21:51:29 +0100

    add lock.sh

--- fzf_run.sh
+++ fzf_run.sh
@@ -4,5 +4,5 @@ selected=$(ls /usr/bin /usr/local/bin | fzf --layout=reverse 2>/dev/null)
 
 if [ -n "$selected" ]; then
   nohup "$selected" >/dev/null 2>&1 &
-  sleep 0.000001
+  sleep 0.1
 fi
---
+++ lock.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+i3lock \
+  --color=292e42 \
+  --keyhl-color=7aa2f7 \
+  --inside-color=1a1b26 \
+  --ring-color=7aa2f7 \
+  --line-color=1a1b26 \
+  --separator-color=1a1b26 \
+  --time-color=c0caf5 \
+  --date-color=c0caf5 \
+  --verif-color=c0caf5 \
+  --wrong-color=ef6155 \
+  --layout-color=c0caf5 \
+  --indicator \
+  --radius=100 \
+  --clock \
